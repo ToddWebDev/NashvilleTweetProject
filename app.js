3e1646ec178101c8dfeda2b445b2f4a6bdaf59c3
ToddWebDev <>
2018-02-14 19:05:22 -0600

    Move Twitter API to api.js

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-var dotenv = require('dotenv').config();
 
 //Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var Twitter = require('twitter');
 
 //Express
 var app = express();
-
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_key_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
 
 //Middleware
 app.use(bodyParser.json());
@@ -25,24 +16,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //API Routes
 app.use('/api', require('./routes/api'));
-
-
-
-//Twitter API Reference Index https://developer.twitter.com/en/docs/api-reference-index
-client.get('search/tweets', {
-	q: 'nashville', 
-	count: 5,
-	geocode: '36.174465,-86.767960,10mi', 
-	result_type: 'recent', 
-	lang: 'en'
-}, function(error, response) {
-  if (!error) {
-    console.log(response);
-    // do stuff here later!
-  } else {
-    console.log(response);
-	}
-});
 
 //Start Server
 app.listen(process.env.PORT || 5000, function () {
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,18 @@
+var dotenv = require('dotenv').config();
+
 //Dependencies
 var express = require('express');
 var router = express.Router();
+var Twitter = require('twitter');
 
-global.tweets = [
-	{
-		text: 'This is a tweet',
-		handle: 'twitterAPI'
-	}
-];
+global.tweets = [];
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_key_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 //Routes
 router.get('/', function(req, res){
@@ -19,3 +24,20 @@ router.get('/', function(req, res){
 
 //Return Router
 module.exports = router;
+
+//Twitter API Reference Index https://developer.twitter.com/en/docs/api-reference-index
+client.get('search/tweets', {
+	q: 'nashville', 
+	count: 5,
+	geocode: '36.174465,-86.767960,10mi', 
+	result_type: 'recent', 
+	lang: 'en'
+}, function(error, response) {
+  if (!error) {
+    console.log(response);
+		global.tweets = response;
+    // do stuff here later!
+  } else {
+    console.log(response);
+	}
+});
